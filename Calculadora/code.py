d87011c4eb4cdefdf9e547f4a49ca2ef424f1cfe
cmlramirez <64171443+cmlramirez@users.noreply.github.com>
2020-04-22 18:08:41 -0600

    Update code.py

--- Calculadora/code.py
+++ Calculadora/code.py
@@ -1,31 +1,25 @@
 from calculadora import *
 
-def esnumero(candidato):
-    valido=True
-    for caracter in candidato :
-        if caracter == "1" or caracter == "2" or caracter == "3" or caracter == "4" or caracter == "5" or caracter == "6" or caracter == "7" or caracter == "8" or caracter == "9" or caracter == "0" :
-            continue
-        else :
-            valido=False
-            break
-    return valido
-    
 print("Bienvenido a la calculadora")
-operacion=input("Indique la operación (+, -, *, /): ")
-oper1=input("Indique el primer número: ")
-oper2=input("Indique el segundo número: ")
-if esnumero(oper1) and esnumero(oper2) :
-    oper1 = int(oper1)
-    oper2 = int(oper2)
-    if operacion == "+" :
-        print(suma(oper1,oper2))
-    elif operacion == "-" :
-        print(resta(oper1, oper2))
-    elif operacion == "*" :
-        print(multiplicacion(oper1, oper2))
-    elif operacion == "/" :
-        print(division(oper1, oper2))
-    else :
-        print("Error! Eso no es una operación válida!")
-else :
-    print("Error! Ambos números deben tener formato numérico.")
+
+operacion = oper1 = oper2 = "None"
+
+while operacion not in ("+","-", "*", "/") :
+    operacion=input("Indique la operación (+, -, *, /): ")
+
+while not str(oper1).isnumeric() :
+    oper1=input("Indique el primer número: ")
+oper1 = int(oper1)
+
+while not str(oper2).isnumeric() :
+    oper2=input("Indique el segundo número: ")
+oper2 = int(oper2)
+
+if operacion == "+" :
+    print("El resultado de la suma es:", suma(oper1, oper2))
+elif operacion == "-" :
+    print("El resultado de la resta es:", resta(oper1, oper2))
+elif operacion == "*" :
+    print("El resultado de la multiplicación es:", multiplicacion(oper1, oper2))
+elif operacion == "/" :
+    print("El resultado de la división es:", division(oper1, oper2))
