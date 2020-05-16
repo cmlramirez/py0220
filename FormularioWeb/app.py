f55fa7ee43f129a47c73c4702ba20b4084a9d392
cmlramirez <64171443+cmlramirez@users.noreply.github.com>
2020-05-16 16:44:09 -0600

    Changed the folder name and updated code
    Changed the folder name and updated code

--- FormularioWeb/app.py
+++ FormularioWeb/app.py
@@ -2,12 +2,30 @@ from flask import Flask, render_template, request, redirect, url_for
 from wtforms import StringField, SubmitField, PasswordField
 from flask_wtf import FlaskForm
 from config import Config
+import psycopg2
 
 class Form(FlaskForm):
     name = StringField('Name')
     password = PasswordField('Password')
     email = StringField('Email')
     submit = SubmitField('Submit')
+
+def AddDBEntry(nam, ema, pas):
+    conn=psycopg2.connect("host=hagrid.delhelsa.com user=python password=greencore dbname=py09")
+    cursor=conn.cursor()
+    cursor.execute("INSERT INTO webform VALUES (%s, %s, %s)",(nam, ema, pas))
+    conn.commit()
+    conn.close()
+
+def CountEntries():
+    conn=psycopg2.connect("host=hagrid.delhelsa.com user=python password=greencore dbname=py09")
+    cursor=conn.cursor()
+    cursor.execute("SELECT COUNT(*) FROM webform")
+    count=cursor.fetchone()
+    conn.close()
+    return count[0]
+
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -16,8 +34,8 @@ app.config.from_object(Config)
 def index():
     page = request.args.get('page', 1)
     list = request.args.get('list', 20)
-    ...
-    return render_template("index.html", num_posts=len(posts))
+
+    return render_template("index.html", num_count=CountEntries())
 
 @app.route("/signup/", methods=["GET", "POST"])
 def show_signup_form():
@@ -26,6 +44,7 @@ def show_signup_form():
         email = request.form['email']
         password = request.form['password']
         next = request.args.get('next', None)
+        AddDBEntry(name, email, password)
         if next:
             return redirect(next)
         return redirect(url_for('index'))
@@ -33,3 +52,4 @@ def show_signup_form():
 
 if __name__ == '__main__':
     app.run()
+
